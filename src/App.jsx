87b7db69189d0067be3caec89f136a4714f41fc8
JohnSloan8 <johnsloan1984@gmail.com>
2022-02-16 23:08:01 +0000

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import * as React from "react";
-import { Link, Outlet } from "react-router-dom"
-import { Navbar, Nav, Container, Row, Col } from "react-bootstrap"
+import * as React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 
 export default function App() {
   return (
-
     <>
       <Navbar fixed="top" bg="light" expand="sm">
         <Container>
@@ -15,25 +14,40 @@ export default function App() {
               width="30"
               height="30"
               className="d-inline-block align-top"
-            /></Navbar.Brand>
+            />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Item>
-                <Nav.Link as={Link} to="/courses">Cúrsaí</Nav.Link>  
+                <Nav.Link as={Link} to="/courses">
+                  Cúrsaí
+                </Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link as={Link} to="/players">Imreoirí</Nav.Link>
+                <Nav.Link as={Link} to="/players">
+                  Imreoirí
+                </Nav.Link>
               </Nav.Item>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       {/* <Container fluid style={{height: '100%', padding: '0', bottom: '0', border: '1px dashed black'}}> */}
-        <Container style={{maxWidth: '767px', bottom: '0', height: '100%', padding: '0', paddingTop: '90px', overflowY: "scroll"}}>
-          <Outlet />
-        </Container>
+      <Container
+        style={{
+          maxWidth: '767px',
+          bottom: '-90px',
+          height: '100%',
+          padding: '0',
+          marginTop: '90px',
+          overflowY: 'scroll',
+          backgroundColor: '#eeeeee',
+        }}
+      >
+        <Outlet />
+      </Container>
       {/* </Container> */}
-    </> 
+    </>
   );
 }
